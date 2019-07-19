3bde8ee1415bfa6eacd8b995de2e7fb7dd1f8d25
Oleksandr Anufriyev <oanufriyev@gmail.com>
2019-07-19 07:57:47 +0200

    Update get_data_for_year_in_csv.py

--- data/truefx_downloader/get_data_for_year_in_csv.py
+++ data/truefx_downloader/get_data_for_year_in_csv.py
@@ -1,6 +1,17 @@
+"""
+You can download all tick data for given year. Data are downloaded for each month, extracted and concatenated into one CSV file.
+python get_data_for_year_in_csv.py -u <truefxUsername> -p <truefxPassword> -f <folder> -y <year> -s <symbol>
+There are some configuration variables in this script:
+    -u or --username - username to login in TrueFX
+    -p or --password - password to login in TrueFX
+    -f or --folder - folder where to download data
+    -y or --year - year for which to download data
+    -s or --symbol - symbol for which to download data
+"""
+
 import errno
 import sys, getopt
-from truefx import manager
+import truefx_downloader
 
 
 def print_usage():
