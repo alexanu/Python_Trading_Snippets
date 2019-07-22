dc53abc46485094762c41438583aa11e982f01a0
Oleksandr Anufriyev <oanufriyev@gmail.com>
2019-07-22 14:49:17 +0200

    Update Time_snippets.py

--- Time_snippets.py
+++ Time_snippets.py
@@ -1,4 +1,4 @@
-from datetime import timezone, datetime
+import datetime
 
 def utc_to_local(self, utc_dt):
     utc_dt = datetime.strptime(utc_dt, "%Y-%m-%d %H:%M:%S")
@@ -11,4 +11,23 @@ def utc_to_local(utc_dt):
 
 utc_dt = datetime.strptime("2018-11-01 01:45:00", "%Y-%m-%d %H:%M:%S")
 print(utc_to_local(utc_dt))
+
+
+def days_ago(d, start_date=None):
+    """
+    get the date string for d days ago
+    """
+    #today is August 13, 10 days ago means August 3
+    if start_date==None:
+        date = datetime.datetime.today() - datetime.timedelta(days=d)
+    else:
+        date = str_to_date(start_date) - datetime.timedelta(days=d)
+    return date.strftime("%Y-%m-%d")
+
+def str_to_date(dt):
+    """
+    convert date string to datetime.date object
+    """
+    year, month, day = (int(x) for x in dt.split('-'))    
+return datetime.date(year, month, day)
 
