123dc04bff24f8b31f6a29851a2b9c8fc0c3c441
Oleksandr Anufriyev <oanufriyev@gmail.com>
2019-09-02 14:12:54 +0200

    Update Performance.py

--- Performance.py
+++ Performance.py
@@ -19,10 +19,17 @@ class Performance(object):
         self.data = pd.DataFrame(data)
         self.ann_factor = ann_factor
 
-    def annualised_return(self):
-        return self._fmt_result(self._ann_return(),
-                                PerfStats.ANN_RETURN.value)
+    def _fmt_result(self, result, name):
+        return result.to_frame(name).T
 
+    
+    def _ann_return(self):
+        return self.data.pct_change().mean().mul(self.ann_factor)    
+    def annualised_return(self):
+        return self._fmt_result(self._ann_return(), PerfStats.ANN_RETURN.value)
+    
+    def _ann_vol(self):
+        return self.data.pct_change().std().mul(self.ann_factor ** 0.5)
     def annualised_vol(self):
         return self._fmt_result(self._ann_vol(), PerfStats.ANN_VOL.value)
 
@@ -30,11 +37,14 @@ class Performance(object):
         result = self._ann_return().div(self._ann_vol())
         return self._fmt_result(result, PerfStats.SHARPE.value)
 
+    
+    def _max_dd(self):
+        return self.drawdown().min().mul(-1)
     def drawdown(self):
         return self.data.div(self.data.cummax()).sub(1)
-
     def max_drawdown(self):
         return self._fmt_result(self._max_dd(), PerfStats.MAX_DD.value)
+
 
     def calmar(self):
         result = self._ann_return().div(self._max_dd())
@@ -48,31 +58,16 @@ class Performance(object):
         result = self.data.pct_change().kurt()
         return self._fmt_result(result, PerfStats.KURTOSIS.value)
 
+    
     def summary(self):
-        funcs = [self.annualised_return, self.annualised_vol,
-                 self.sharpe, self.max_drawdown, self.calmar,
-                 self.skewness, self.kurtosis]
+        funcs = [self.annualised_return, 
+                 self.annualised_vol,
+                 self.sharpe, 
+                 self.max_drawdown, 
+                 self.calmar,
+                 self.skewness, 
+                 self.kurtosis]
         return pd.concat([f() for f in funcs], axis=0)
-
-    # def _clean_summary(self, summary):
-    #     return summary.style.format({
-    #         PerfStats.ANN_RETURN.value: "{:.2%}",
-    #         PerfStats.ANN_VOL.value: "{:.2%}",
-    #         PerfStats.SHARPE.value: "{:.2}",
-    #         PerfStats.MAX_DD.value: "{:.2%}",
-    #         PerfStats.CALMAR.value: "{:.2}",
-    #         PerfStats.SKEWNESS.value: "{:.2}",
-    #         PerfStats.KURTOSIS.value: "{:.2}"
-    #     })
-
-    def _ann_return(self):
-        return self.data.pct_change().mean().mul(self.ann_factor)
-
-    def _ann_vol(self):
-        return self.data.pct_change().std().mul(self.ann_factor ** 0.5)
-
-    def _max_dd(self):
-        return self.drawdown().min().mul(-1)
-
-    def _fmt_result(self, result, name):
-return result.to_frame(name).T
+    
+    
+    
