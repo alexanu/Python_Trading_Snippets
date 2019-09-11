caf47df1761168ff1ef20f1759cd0217f58ac08e
Oleksandr Anufriyev <oanufriyev@gmail.com>
2019-09-11 13:56:52 +0200

    Update snippets.py

--- snippets.py
+++ snippets.py
@@ -209,6 +209,43 @@ while nse_stocks_page.has_more_results() and pageCount < 7: # restricting the pa
     pageCount = pageCount + 1
     nse_stocks_page = nse.datasets(params = {"page":pageCount})
 
+	
+#------------------------------------------------------------------------------------------------------------------------------
+	
+
+import os	
+import pandas.io.data as web
+import datetime as dt
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def download_SPX(date=None):
+    if date == None:
+        date = dt.date.today()
+
+    s_dir = os.getcwd() + '/' + date.strftime('%Y-%m-%d')
+    if not os.path.exists(s_dir):
+        os.makedirs(s_dir)
+
+    start = dt.datetime(1990,1,1)
+    end = date
+    f = web.DataReader("^GSPC", 'yahoo', start, end).fillna('NaN')
+    f.to_csv(s_dir+'/SPX.csv', date_format='%Y%m%d')	
+	
+def main():
+
+    sched = BlockingScheduler()
+
+    @sched.scheduled_job('cron', day_of_week='mon,tue,wed,thu,fri,sat', hour=23)
+    def scheduled_job():
+    	print('[INFO] Job started.')
+        download_SPX()
+        print('[INFO] Job ended.')
+
+    sched.start()    
+
+if __name__ == '__main__':
+	main()
+	
 #------------------------------------------------------------------------------------------------------------------------------
 
 # uses Yahoo Finance's API to get minute-by-minute ticks
